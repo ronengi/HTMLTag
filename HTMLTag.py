ae6761e3140d0e1a23f8f4d8eaab2455ebf27e3f
Ronen Gilead-Raz <ronengi@gmail.com>
2017-06-01 17:48:38 +0300

    First working version.
        Recursively go through dict object and translate it to various html elements.

--- HTMLTag.py
+++ HTMLTag.py
@@ -1,76 +1,79 @@
 #!/usr/bin/python
 
 
-# todo: implement a separate <div> creator
-# todo: after that implement a general <tag> creator
-
 class HTMLTag:
-    """Generate html code for tag creation.
-    
-    info:   dictionary with all necessary tag data
-            t_type:         the html tag type
-            t_id            the tag id
-            t_classes       list of all tag classes
-            t_styles        list of all inline style details
-            t_properties    additional tag properties
-            t_contents      the text inside the tag
-    
+    """Performs various actions related to html tags.
+
+    generate()  create html tag from dictionary object.
     """
 
     @staticmethod
-    def generate_html(info={}):
+    def get_info(info=None, taglines=[]):
+        # print(info)
+        if info.__class__.__name__ in ('dict'):
+            for key, val in info.items():
+                # todo: future key could be a tuple
+                if key in ('html', 'body', 'div', 'span', 'p'):
+                    taglines.append('<{0}'.format(key))
+                    HTMLTag.get_info(val, taglines)
+                    taglines.append('</{0}>'.format(key))
+                elif key in ('t_id'):
+                    # val must be str
+                    taglines.append('id="{0}"'.format(val))
+                elif key in ('t_classes'):
+                    # val must be sub-list
+                    classlines = ['class="']
+                    for item in val:
+                        classlines.append('{0} '.format(item))
+                    classlines.append('"')
+                    taglines.append(''.join(classlines))
+                elif key in ('t_styles'):
+                    # val must be sub-dictionary
+                    stylelines = ['style="']
+                    for subkey, subval in val.items():
+                        stylelines.append('{0}: {1}; '.format(subkey, subval))
+                    stylelines.append('"')
+                    taglines.append(''.join(stylelines))
+                elif key in ('left', 'top', 'right', 'bottom', 'width', 'height'):
+                    taglines.append('{0}: {1};'.format(key, val))
+                elif key in ('t_properties'):
+                    # val must be sub-dictionary
+                    for subkey, subval in val.items():
+                        taglines.append('{0}="{1}"'.format(subkey, subval))
+                elif key in ('t_contents'):
+                    taglines.append('>')
+                    # val must be a list
+                    for item in val:
+                        HTMLTag.get_info(item, taglines)
+                else:
+                    taglines.append('{0}:'.format(str(key)))
+                    HTMLTag.get_info(val, taglines)
+                    taglines.append('/{0}'.format(str(key)))
+
+        elif info.__class__.__name__ in ('list'):
+            for item in info:
+                HTMLTag.get_info(item, taglines)
+        else:
+            taglines.append('{0}'.format(str(info)))
+
+    @staticmethod
+    def generate(info: dict = {}) -> str:
+        """Generate html code for tag creation.
+
+        :return: string of html tag ready to display.
+        :argument info: dictionary with all necessary tag data
+                        t_type:         the html tag type
+                        t_id            the tag id
+                        t_classes       list of all tag classes
+                        t_styles        list of all inline style details
+                        t_properties    additional tag properties
+                        t_contents      the text inside the tag
         """
-            div = '<div id="it_id_7" class="InfoTom lang_rtl" style="left: 10px; top: 110px; width: 250px; height: 20px;" draggable="true">/usr/share/vim/vim80/vimrc_example.vim
-                <div id="rh_it_id_7" class="resizeHandle" style="right: 1px; bottom: 1px;" draggable="true"> &square; </div>
-            </div>
-        """
-        tag = {'t_id': self.__infotom.get_it_id()
-               , 't_classes': []
-               , 't_styles': []
-               , 't_properties': []
-               , 't_contents': []}
 
-        if it.get_lang_rtl():
-            html_div['classes'].append('lang_rtl')
-
-        html_div['styles'].append('left: {0}px;'.format(str(it.get_left())))
-        html_div['styles'].append('top: {0}px;'.format(str(it.get_top())))
-        html_div['styles'].append('width: {0}px;'.format(str(it.get_width())))
-        html_div['styles'].append('height: {0}px;'.format(str(it.get_height())))
-
-        if isinstance(it, InfoTom):
-            html_div['classes'].append('InfoTom')
-            html_div['properties'].append('draggable="true"')
-            html_div['contents'].append(str(it))
-            html_div['contents'].append(WebInfoTom.add_resize_handle(it_id=html_div['it_id']))
-
-        tag = '<{0} id="{1}" class="{2}" style="{3}" {4}>{5}</{0}>'.format(
-            info['t_type']
-            , info['t_id']
-            , ' '.join(info['t_classes'])
-            , ' '.join(info['t_styles'])
-            , ' '.join(info['t_properties'])
-            , '\n'.join(info['t_contents']))
-
-        return tag
-
-
-        div = '<div id="{0}" class="{1}" style="{2}" {3}>{4}</div>'.format(
-            html_div['it_id']
-            , ' '.join(html_div['classes'])
-            , ' '.join(html_div['styles'])
-            , ' '.join(html_div['properties'])
-            , '\n'.join(html_div['contents']))
-
-        return div
-
-tag = dict(
-    t_type='div'
-    , t_id='it_id_1'
-    , t_classes=['class1', 'class2', 'class3']
-    , t_styles={'left': '12px', 'top': '13px'}
-    , t_properties={'draggable': 'true'}
-    , t_contents='the text inside the tag')
-
-print(HTMLTag.generate_html(tag))
+        taglines = []
+        HTMLTag.get_info(info, taglines)
+        # print(info, '<br>\n')
+        # for item in taglines:
+        #     print('{0}<br>\n'.format(item))
+        return ' '.join(taglines)
 
